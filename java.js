17b539e02d8fc92dde374f599eae20d3fd6f2c43
Muhammed Ramazan Çolak <muhammedramazancolak@gmail.com>
2025-05-08 23:28:52 +0300

    navbar güncellendi

--- java.js
+++ java.js
@@ -1,25 +1,44 @@
+
+const hukuklar = ["miras", "iş", "idare", "icraiflas", "aile", "ceza", "tüketici"]
+
+// document.addEventListener("click", (e) => {
+//   let condition = false;
+//   for (const hukuk of hukuklar) {
+//     console.log(hukuk);
+//     const endswith = window.location.href.endsWith(("#" + hukuk));
+//     console.log(endswith)
+//     if (endswith) {
+//       condition = true;
+//       break;
+//     }
+//   }
+//   if (condition) {
+//     window.scrollTo(window.scrollY - 162);
+//   }
+// })
+
 function toggleDropdown() {
-    const menu = document.getElementById('dropdownMenu');
-    menu.classList.toggle('hidden');
+  const menu = document.getElementById('dropdownMenu');
+  menu.classList.toggle('hidden');
+}
+
+document.addEventListener('click', function (e) {
+  const button = document.querySelector('button[onclick="toggleDropdown()"]');
+  const menu = document.getElementById('dropdownMenu');
+  if (!button.contains(e.target) && !menu.contains(e.target)) {
+    menu.classList.add('hidden');
   }
+});
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
 
-  document.addEventListener('click', function (e) {
-    const button = document.querySelector('button[onclick="toggleDropdown()"]');
-    const menu = document.getElementById('dropdownMenu');
-    if (!button.contains(e.target) && !menu.contains(e.target)) {
-      menu.classList.add('hidden');
-    }
-  });
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-    menuToggle.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
-
-    ScrollReveal().reveal('.service-card', {
-      delay: 200,
-      distance: '100px',
-      origin: 'bottom',
-      interval: 500,
-      duration: 2000,
-    });
+ScrollReveal().reveal('.service-card', {
+  delay: 200,
+  distance: '100px',
+  origin: 'bottom',
+  interval: 500,
+  duration: 2000,
+});
